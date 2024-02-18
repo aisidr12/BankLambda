ef657e25a1f1f374df28e1ace8c822444544d2dc
Arturo <art2dit2@hotmail.com>
2024-02-18 19:22:37 +0100

    Set fully formula

--- src/main/java/com/lambda/BankRequest.java
+++ src/main/java/com/lambda/BankRequest.java
@@ -1,2 +1,34 @@
-package com.lambda;public class BankRequest {
+package com.lambda;
+
+import java.math.BigDecimal;
+
+
+public class BankRequest {
+    private BigDecimal amount;
+    private BigDecimal rate;
+    private Integer term;
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    public Integer getTerm() {
+        return term;
+    }
+
+    public void setTerm(Integer term) {
+        this.term = term;
+    }
 }
--- src/main/java/com/lambda/LambdaBank.java
+++ src/main/java/com/lambda/LambdaBank.java
@@ -1,13 +1,68 @@
 package com.lambda;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-@SpringBootApplication
-public class JavaLambdaApplication {
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
-    public static void main(String[] args) {
-        SpringApplication.run(JavaLambdaApplication.class, args);
+
+public class LambdaBank implements RequestHandler<BankRequest,BankResponse> {
+/*
+P = MONTO DEL PRESTAMO
+I = TASA DE INTEREES MENSUAL
+N = PLAZO DEL CREDIO EN MESES
+
+FORMULA para obtener la cuota mensual  ===>   (P*I)/(1- (1+i)^(-n))
+ */
+
+    @Override
+    public BankResponse handleRequest(BankRequest bankRequest, Context context) {
+        context.getLogger().log("Getting the request "+ bankRequest.toString());
+        MathContext mathContext = MathContext.DECIMAL128;
+        //amount
+        BigDecimal amount = bankRequest.getAmount().setScale(2, RoundingMode.HALF_UP);
+        //rate (tasa in spanish)
+        BigDecimal monthlyRate  = bankRequest.getRate()
+                .setScale(2, RoundingMode.HALF_UP)
+                .divide(BigDecimal.valueOf(100), mathContext);
+        //rate when is a registered client
+        BigDecimal monthlyRateWithAccount  = bankRequest.getRate()
+                .subtract(BigDecimal.valueOf(0.2),mathContext)
+                .setScale(2,RoundingMode.HALF_UP)
+                .divide(BigDecimal.valueOf(100), mathContext);
+        Integer term = bankRequest.getTerm();
+
+        BigDecimal monthlyPayment = this.calculateQuota(amount, monthlyRate, term, mathContext);
+        BigDecimal monthlyPaymentWithAccount = this.calculateQuota(amount, monthlyRateWithAccount, term, mathContext);
+
+        BankResponse bankResponse = new BankResponse();
+        bankResponse.setQuota(monthlyPayment);
+        bankResponse.setRate(monthlyRate);
+        bankResponse.setTerm(term);
+        bankResponse.setQuotaWithAccount(monthlyPaymentWithAccount);
+        bankResponse.setRateWithAccount(monthlyRateWithAccount);
+        bankResponse.setTermWithAccount(term);
+
+        return bankResponse;
     }
 
+    private BigDecimal calculateQuota(BigDecimal amount,BigDecimal rate, Integer term,MathContext mathContext){
+       //  (P*I)/(1- (1+i)^(-n))
+//        P = MONTO DEL PRESTAMO
+//        I = TASA DE INTEREES MENSUAL
+//        N = PLAZO DEL CREDIO EN MESES
+        BigDecimal onePlusRate = rate.add(BigDecimal.ONE, mathContext);
+        // calculate (1+i)^ -n
+        BigDecimal onePlusRateToN = onePlusRate.negate().pow(term,mathContext);
+        BigDecimal onePlusRateToNegative = BigDecimal.ONE.divide(onePlusRate,mathContext);
+        //calculate monthly quota
+        BigDecimal numerator = amount.multiply(rate, mathContext);
+        BigDecimal denominator = BigDecimal.ONE.subtract(onePlusRateToNegative, mathContext);
+        BigDecimal monthlyPayment = numerator.divide(denominator, mathContext);
+        // Set the result to 2 decimals
+        monthlyPayment = monthlyPayment.setScale(2, RoundingMode.HALF_UP);
+        return monthlyPayment;
+    }
 }
