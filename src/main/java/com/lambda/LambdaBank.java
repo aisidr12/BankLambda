13126adeaf2de29465ca73b94271bde9dbc749e6
Arturo <art2dit2@hotmail.com>
2024-03-03 17:42:05 +0100

    Revert last commit

--- src/main/java/com/lambda/LambdaBank.java
+++ src/main/java/com/lambda/LambdaBank.java
@@ -19,8 +19,6 @@ FORMULA para obtener la cuota mensual  ===>   (P*I)/(1- (1+i)^(-n))
 
     @Override
     public BankResponse handleRequest(BankRequest bankRequest, Context context) {
-        String awsRequestId = context.getAwsRequestId();
-        System.out.println("Checking whats is awsRequestId  "+ awsRequestId);
         context.getLogger().log("Getting the request "+ bankRequest.toString());
         MathContext mathContext = MathContext.DECIMAL128;
         //amount
